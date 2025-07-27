7a0a12131c1110f1a8bb8979713d478ceae22a53
Ighor Oliveira <ighor9991@gmail.com>
2025-07-27 20:02:10 -0300

    trioed to fix the redirect problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react";
 import Login from "./components/Login";
+import { getRedirectResult } from "firebase/auth";
+import { auth } from "../firebase";
 
 
 function App() {
+
+  useEffect(() => {
+    console.log("🔍 Checking for redirect login result...");
+    getRedirectResult(auth)
+      .then((result) => {
+        if (result?.user) {
+          console.log("✅ Logged in user:", result.user);
+        } else {
+          console.log("⛔ No user returned from redirect");
+        }
+      })
+      .catch((error) => {
+        console.error("🔥 Error during login redirect:", error);
+      });
+  }, []);
+
   return(
     <div className="w-screen h-screen bg-gray-100 flex justify-center items-center">
       <Login/>
